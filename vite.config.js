777763699286ec7c2d1447a98e58bddc00764f0d
shysha <shyshasy@gmail.com>
2024-09-19 16:42:50 +0000

    add versel commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist' // Assurez-vous que ce répertoire correspond à ce que vous utilisez dans Vercel
   }
-})
+});
