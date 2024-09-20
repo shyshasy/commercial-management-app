ad066598c8d45f6e9c5e09570cc0222b78091ce4
shysha <shyshasy@gmail.com>
2024-09-20 12:16:10 +0000

    feat: Add meaningful title to index.html

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <router-link class="navbar-brand" to="/">ABC Corporation</router-link>
+        <router-link class="navbar-brand" to="/">Home</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
